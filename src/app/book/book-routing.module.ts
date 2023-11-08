ed03d4a6190cd9be7fd1d785c0972592cf30d028
mehdi-mtir <mehdi.mtir@gmail.com>
2023-11-08 16:39:20 +0100

    edit book

--- src/app/book/book-routing.module.ts
+++ src/app/book/book-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListBookComponent } from './list-book/list-book.component';
 import { AddBookComponent } from './add-book/add-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
 
 const routes: Routes = [
   {path:'', component : ListBookComponent},
-  {path:'add', component: AddBookComponent}
+  {path:'add', component: AddBookComponent},
+  {path:'edit/:id', component:EditBookComponent}
 ];
 
 @NgModule({
--- src/app/book/edit-book/edit-book.component.ts
+++ src/app/book/edit-book/edit-book.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../services/book.service';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
   styleUrls: ['./edit-book.component.css']
 })
-export class EditBookComponent {
+export class EditBookComponent implements OnInit {
+  book? : Book;
+  editForm = this.fb.group(
+    {
+      title : ['', Validators.required],
+      author : [''],
+      price : ['']
+    }
+  )
+  constructor(
+    private fb : FormBuilder,
+    private activatedRoute : ActivatedRoute,
+    private service : BookService,
+    private router : Router){}
+
+  editBook = ()=>{
+    this.service.editBook(
+      new Book(this.book!.id,
+        this.editForm.value.title!,
+        this.editForm.value.author!,
+        +this.editForm.value.price!)
+    )
+    this.router.navigate(['/book']);
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      parametres => {
+        //console.log(parametres['id']);
+        this.book = this.service.getBookById(+parametres['id']);
+        this.editForm.setValue(
+          {
+            title : this.book!.title,
+            author : this.book!.author,
+            price : this.book!.price+''
+          }
+        )
+      }
+    )
+  }
 
 }
